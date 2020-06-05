737a65363223b635ac31b9cc8e0f089011b6929f
bakamoridesu <bakamoridesu@gmail.com>
2020-06-05 16:37:44 +0300

    adding state

--- contact_list/reactnd-contacts-app/src/App.js
+++ contact_list/reactnd-contacts-app/src/App.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-];
-
-function App () {
-    return (
-      <div>
-        <ListContacts contacts={contacts} asd={'sdf'}/>
-      </div>
-    );
+class App extends Component {
+	state = {
+		contacts: [
+					 {
+					   "id": "karen",
+					   "name": "Karen Isgrigg",
+					   "handle": "karen_isgrigg",
+					   "avatarURL": "http://localhost:5001/karen.jpg"
+					 },
+					 {
+					   "id": "richard",
+					   "name": "Richard Kalehoff",
+					   "handle": "richardkalehoff",
+					   "avatarURL": "http://localhost:5001/richard.jpg"
+					 },
+					 {
+					   "id": "tyler",
+					   "name": "Tyler McGinnis",
+					   "handle": "tylermcginnis",
+					   "avatarURL": "http://localhost:5001/tyler.jpg"
+					 }
+				]
+	}
+	render(){
+		return (
+		  <div>
+			<ListContacts contacts={this.state.contacts} asd={'sdf'}/>
+		  </div>
+		)
+	}
 }
 
 export default App;
