aff450ba4fc3be15f41b4a855ad6a0fac3206a39
bakamoridesu <bakamoridesu@gmail.com>
2020-06-06 17:19:01 +0300

    update UI based on form state

--- contact_list/reactnd-contacts-app/src/ListContacts.js
+++ contact_list/reactnd-contacts-app/src/ListContacts.js
@@ -15,6 +15,15 @@ class ListContacts extends Component{
 		})
 	}
 	render(){
+		
+		const {query} = this.state
+		const {contacts, onDeleteContact} = this.props
+		const newContacts = query === '' 
+		? contacts
+		: contacts.filter((c) => (
+			c.name.toLowerCase().includes(query.toLowerCase())
+		))
+		
 		return (
 		  <div className = 'list-contacts'>
 		    <div className='list-contacts-top'>
@@ -22,12 +31,12 @@ class ListContacts extends Component{
 					className='search-contacts'
 					type='text'
 					placeholder='Search Contacts'
-					value={this.state.query}
+					value={query}
 					onChange={(event) => this.updateQuery(event.target.value)}
 				/>
 			</div>
 			<ol className='contact-list'>
-			{this.props.contacts.map(contact => (
+			{newContacts.map(contact => (
 				<li key={contact.id} className='contact-list-item'>
 					<div 
 						className='contact-avatar' 
@@ -38,7 +47,7 @@ class ListContacts extends Component{
 						<p>{contact.name}</p>
 						<p>{`@${contact.handle}`}</p>
 					</div>
-					<button className='contact-remove' onClick={() => this.props.onDeleteContact(contact)}>
+					<button className='contact-remove' onClick={() => onDeleteContact(contact)}>
 					Remove
 					</button>
 				</li>
