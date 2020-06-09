7229c6051315d33e414e17a63efdf4f8d63858da
bakamoridesu <bakamoridesu@gmail.com>
2020-06-09 14:54:33 +0300

    create contact static functional

--- contact_list/reactnd-contacts-app/src/CreateContact.js
+++ contact_list/reactnd-contacts-app/src/CreateContact.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
 	render(){
 		return(
-			<h1> Create contact </h1>
+			<div>
+				<Link className = 'close-create-contact'
+				to='/'>
+					Close
+				</Link>
+				<form className='create-contact-form'>
+					<ImageInput
+						className='create-contact-avatar-input'
+						name='avatarURL'
+						maxHeight={64}
+					/>
+					<div className='create-contact-details'>
+						<input type='text' name='name' placeHolder='Name'/>
+						<input type='text' name='handle' placeHolder='Handle'/>
+						<button>Add Contact</button>
+					</div>
+				</form>
+			</div>
 		)
 	}
 }
