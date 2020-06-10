995cfe54a9abbad40d40490b8d804714e193c8e9
bakamoridesu <bakamoridesu@gmail.com>
2020-06-10 23:36:48 +0300

    create store from scratch

---
+++ store/index.js
@@ -0,0 +1,34 @@
+// Library code
+function createStore(reducer) {
+	
+	let state
+	
+	const getState = () => state
+	
+	const listeners = []
+	
+	const subscribe((listener) => {
+		listeners.push(listener)
+		return () => {
+			listeners.filter((l) => l !== listener)
+		}
+	})
+	
+	const dispatch = (action) => {
+		state = todos(state, action)
+		listeners.forEach((listener) => listener())
+	}
+	return {
+		getState,
+		subscribe,
+		dispatch,
+	}
+}
+
+// App code
+function todos (state=[], action) {
+	if(action.type === 'ADD_TODO'){
+		return state.concat([action.todo])
+	}
+	return state
+}
