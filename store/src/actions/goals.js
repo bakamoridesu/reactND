22ed93106ecaa656bf8318682f4f52b0cfb876c3
bakamoridesu <bakamoridesu@gmail.com>
2020-06-16 04:28:45 +0300

    move actions code to actions folder

---
+++ store/src/actions/goals.js
@@ -0,0 +1,40 @@
+import API from 'goals-todos-api'
+
+export const ADD_GOAL = 'ADD_GOAL'
+export const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addGoal(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoal(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+export function handleRemoveGoalAction(goal) {
+  return (dispatch) => {
+    dispatch(removeGoal(goal.id))
+
+    API.deleteGoal(goal.id).catch(() => {
+      dispatch(addGoal(goal));
+      alert('An error occured. Try again.');
+    })
+  }
+}
+
+export function handleAddGoalAction(goalName, cb) {
+  return (dispatch) => {
+    API.saveGoal(goalName)
+      .then((goal) => {
+        dispatch(addGoal(goal))
+        cb()
+      })
+      .catch(() => alert('An error occured. Try again'))
+  }
+}
