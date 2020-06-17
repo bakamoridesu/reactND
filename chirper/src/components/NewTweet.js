726aba8be2fd8aa88db5a47bdf56ef0470b668a7
bakamoridesu <bakamoridesu@gmail.com>
2020-06-18 02:48:15 +0300

    add add new tweet

--- chirper/src/components/NewTweet.js
+++ chirper/src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet} from "../actions/tweets";
 
 class NewTweet extends Component{
 
@@ -18,8 +20,10 @@ class NewTweet extends Component{
     e.preventDefault()
 
     const { text } = this.state
+    const { dispatch, id } = this.props
 
-    // todo: add Tweet to the store
+    dispatch(handleAddTweet(text, id))
+
 
     this.setState(() => ({
       text: ''
@@ -59,4 +63,4 @@ class NewTweet extends Component{
   }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
