67d28c4bb5d3261ca93be10a5580e366b4ca65dd
bakamoridesu <bakamoridesu@gmail.com>
2020-06-18 01:49:18 +0300

    implement tweet component

--- chirper/src/components/App.js
+++ chirper/src/components/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from "../actions/shared"
 import Dashboard from "./Dashboard";
+import LoadingBar from 'react-redux-loading'
+
+
+
+
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
@@ -10,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar />
         {this.props.loading ? null : <Dashboard />}
       </div>
     )
