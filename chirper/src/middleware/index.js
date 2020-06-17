c65952ab198794d3f155ba5010d65adeddf092b5
bakamoridesu <bakamoridesu@gmail.com>
2020-06-17 09:34:08 +0300

    add middleware

---
+++ chirper/src/middleware/index.js
@@ -0,0 +1,19 @@
+import logger from "./logger";
+import thunk from 'redux-thunk'
+import { applyMiddleware} from "redux";
+
+export default applyMiddleware(
+  thunk,
+  logger,
+)
+
+
+
+
+
+
+
+
+
+
+
