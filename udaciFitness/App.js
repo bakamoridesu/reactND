4832c67c852c62484e19b9197126f3e16b6b9382
bakamoridesu <bakamoridesu@gmail.com>
2020-06-21 06:40:32 +0300

    udaciFitness - initial commit

---
+++ udaciFitness/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Text, View } from 'react-native';
+
+import AddEntry from "./components/AddEntry";
+
+export default function App() {
+  return (
+    <AddEntry />
+  );
+}
